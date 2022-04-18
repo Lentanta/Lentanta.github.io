cc88fe02d0e83afefe36950b5af548c9151e2e26
thanhtam <tam.le@avironactive.com>
2022-04-18 12:02:27 +0700

    Small changes, add Zustand and Styled component

--- src/layouts/InformationLayout/index.tsx
+++ src/layouts/InformationLayout/index.tsx
@@ -6,7 +6,7 @@ const InformationLayout = () => {
     <TabHOC>
       <div style={{ padding: "10px", }}>
         <h2>Hi, my name is Lê Thanh Tâm</h2>
-        <p>Curently, I'm work as a full-stack developer</p>
+        <p>Curently, I'm working as a full-stack developer</p>
         <p>This is my github page: <a href="https://github.com/Lentanta" target="_blank">https://github.com/Lentanta</a></p>
       </div>
     </TabHOC>
